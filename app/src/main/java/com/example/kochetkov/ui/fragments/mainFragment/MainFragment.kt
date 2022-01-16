ccb72a9ceb98521ea8feffd92377868b79b9cd35
Daniil Kochetkov <kochetkovdaniil.s@gmail.com>
2022-01-16 17:23:09 +0300

    1.0

--- app/src/main/java/com/example/kochetkov/ui/fragments/mainFragment/MainFragment.kt
+++ app/src/main/java/com/example/kochetkov/ui/fragments/mainFragment/MainFragment.kt
@@ -1,13 +1,16 @@
 package com.example.kochetkov.ui.fragments.mainFragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.kochetkov.R
 import com.example.kochetkov.databinding.FragmentMainBinding
 import com.example.kochetkov.ui.MainActivity
 import com.example.kochetkov.ui.common.isTablet
@@ -30,6 +33,7 @@ class MainFragment : Fragment(), ImageForwardClick {
         return binding.root
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -42,13 +46,25 @@ class MainFragment : Fragment(), ImageForwardClick {
         binding.imRcView.layoutManager =
             GridLayoutManager(requireContext(), spanCount)
 
+        getImage()
+
+        binding.swipeRefresh.setOnRefreshListener {
+            getImage()
+            adapter.notifyDataSetChanged()
+            Toast.makeText(requireContext(), "Данные обновлены!", Toast.LENGTH_LONG).show()
+            binding.swipeRefresh.isRefreshing = false
+        }
+    }
+
+    private fun getImage(){
         viewModel.getImage().observe(viewLifecycleOwner, { list ->
             list.body()?.let { adapter.setList(it) }
         })
     }
 
     override fun onClick(imageUrl: String) {
-        (requireActivity() as MainActivity).findNavController()
+        val bundle = Bundle()
+        bundle.putString("image", imageUrl)
+        (requireActivity() as MainActivity).findNavController().navigate(R.id.action_mainFragment_to_secondFragment3, bundle)
     }
-
 }
--- app/src/main/java/com/example/kochetkov/ui/fragments/secondFragment/SecondFragment.kt
+++ app/src/main/java/com/example/kochetkov/ui/fragments/secondFragment/SecondFragment.kt
@@ -5,17 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.kochetkov.R
+import com.example.kochetkov.databinding.FragmentMainBinding
+import com.example.kochetkov.databinding.FragmentSecondBinding
 
 
 class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getImage()
+    }
+
+    fun getImage(){
+        val image = arguments?.get("image")
+        binding.apply {
+            Glide.with(this@SecondFragment)
+                .load(image)
+                .centerCrop()
+                .into(imageView)
+        }
+    }
 }
